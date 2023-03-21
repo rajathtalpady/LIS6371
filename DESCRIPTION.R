de32851eefa5966f0cc9ee80a53d12c76ce8a017
rajathtalpady <122480679+rajathtalpady@users.noreply.github.com>
2023-03-21 01:45:30 -0400

    Add files via upload

---
+++ DESCRIPTION.R
@@ -0,0 +1,9 @@
+Package: Genomics
+Title: Tools for analyzing and visualizing genomic data
+Version: 0.1
+Authors@R: "Rajath Talpady <rajatht@usf.edu> [aut, cre]"
+Description: A collection of functions and tools for analyzing and visualizing genomic data, including read alignment, variant calling, and gene expression analysis.
+License: CC0
+Depends: R (>= 3.1.2),
+LazyData: true
+
